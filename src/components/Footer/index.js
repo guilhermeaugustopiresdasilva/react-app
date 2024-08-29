2b7de6fc1de4bdbe42bfd9ba2d9c0b2d5e0d786d
guilhermeaugustopiresdasilva <gm.dasilva007@gmail.com>
2024-08-29 07:41:57 -0300

    react

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+function Footer(){
+    return(
+        <footer className={styles.footer}>
+            <h2>Desenvolvido por Guilherme</h2>
+        </footer>
+    );
+}
+
+export default Footer;
